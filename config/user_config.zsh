10e267c15bf88a72f61a5b3f722104fb92d13733
dong881 <minghunghsu.taiwan@gmail.com>
2025-09-19 09:57:37 +0800

    add alias for M-plane auto configuration script

--- config/user_config.zsh
+++ config/user_config.zsh
@@ -101,3 +101,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # auto M-plane configuration
+alias pegam='/home/oai72_su/PEGA_Mplane/autoMplane.sh'
